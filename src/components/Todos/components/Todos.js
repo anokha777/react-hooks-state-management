461367200252dc2576ef95efff515adb6cb4824f
Ashwani Payal <ashwani.payal@publicissapient.com>
2021-01-17 01:25:46 +0530

    update

--- src/components/Todos/components/Todos.js
+++ src/components/Todos/components/Todos.js
@@ -30,7 +30,7 @@ export default function Todos() {
           <input className={themeClass} type='text' id='add-todo' placeholder='Add Todo...' />
         </form>
       </div>
-      <h1>{initialState.todos.length}</h1>
+      <h1>{initialState.todos.length} Todos</h1>
       <ul className='list-wrapper'>
         {initialState.todos.map((todo) => (
           <li className={`${themeClass} todo-item`} key={todo.id}>
